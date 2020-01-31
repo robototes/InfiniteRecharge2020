96d9732d938ced8124ff7e99fd71efdcb40bbb0b
Eli Orona <eliorona@live.com>
2020-01-30 16:30:44 -0800

    Update ClimbRails.java

--- src/main/java/frc/team2412/robot/Commands/ClimbRails.java
+++ src/main/java/frc/team2412/robot/Commands/ClimbRails.java
@@ -2,25 +2,18 @@ package frc.team2412.robot.Commands;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
-import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.team2412.robot.RobotMap;
 import frc.team2412.robot.Subsystems.ClimbMotorSubsystem;
 
-public class ClimbRails extends CommandBase {
+public class ClimbRails extends SubsystemBase {
 	private WPI_TalonSRX m_leftClimbMotor;
 	private WPI_TalonSRX m_rightClimbMotor;
 
 	public ClimbRails(WPI_TalonSRX leftClimbMotor, WPI_TalonSRX rightClimbMotor) {
-		super();
-
+		m_leftClimbMotor = leftClimbMotor;
+		m_rightClimbMotor = rightClimbMotor;
 	}
-
-	public ClimbRails(ClimbMotorSubsystem m_ClimbMotorSubsystem) {
-		// TODO Auto-generated constructor stub
-	}
-
-	private WPI_TalonSRX leftClimbMotor = RobotMap.leftClimbMotor;
-	private WPI_TalonSRX rightClimbMotor = RobotMap.rightClimbMotor;
 
 	public void climbUp() {
 		leftClimbMotor.set(1.0);
@@ -38,11 +31,5 @@ public class ClimbRails extends CommandBase {
 		leftClimbMotor.set(0.0);
 		rightClimbMotor.set(0.0);
 		System.out.println("Stopped");
-	}
-
-	@Override
-	protected void initDefaultCommand() {
-		// TODO Auto-generated method stub
-		m_ClimbLiftSubsystem.ClimbMotorSubsystem();
 	}
 }
