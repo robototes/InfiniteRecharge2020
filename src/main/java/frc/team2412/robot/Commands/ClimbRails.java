4fc06ce3f5e54d1072a06499262fa14d8f70a905
Eli Orona <eliorona@live.com>
2020-02-03 11:02:32 -0800

    Update ClimbRails.java

--- src/main/java/frc/team2412/robot/Commands/ClimbRails.java
+++ src/main/java/frc/team2412/robot/Commands/ClimbRails.java
@@ -3,40 +3,17 @@ package frc.team2412.robot.Commands;
 import com.revrobotics.CANSparkMax;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.team2412.robot.RobotMap;
 import frc.team2412.robot.Subsystems.ClimbMotorSubsystem;
 
-<<<<<<< HEAD
-public class ClimbRails extends CommandBase {
-	//private CANSparkMax m_leftClimbMotor;
-	//private CANSparkMax m_rightClimbMotor;
-=======
 public class ClimbRails extends SubsystemBase {
-	private WPI_TalonSRX m_leftClimbMotor;
-	private WPI_TalonSRX m_rightClimbMotor;
->>>>>>> branch 'master' of https://github.com/AriGlockner/InfiniteRecharge2020-1.git
+	private CANSparkMax m_leftClimbMotor;
+	private CANSparkMax m_rightClimbMotor;
 
-<<<<<<< HEAD
-	/*public ClimbRails(CANSparkMax leftClimbMotor, CANSparkMax rightClimbMotor) {
-		super();
-
-=======
-	public ClimbRails(WPI_TalonSRX leftClimbMotor, WPI_TalonSRX rightClimbMotor) {
+	public ClimbRails(CANSparkMax leftClimbMotor, CANSparkMax rightClimbMotor) {
 		m_leftClimbMotor = leftClimbMotor;
 		m_rightClimbMotor = rightClimbMotor;
->>>>>>> branch 'master' of https://github.com/AriGlockner/InfiniteRecharge2020-1.git
 	}
 
-<<<<<<< HEAD
-	public ClimbRails(ClimbMotorSubsystem m_ClimbMotorSubsystem) {
-		// TODO Auto-generated constructor stub
-	}
-	
-	private CANSparkMax leftClimbMotor = RobotMap.leftClimbMotor;
-	private CANSparkMax rightClimbMotor = RobotMap.rightClimbMotor;
-
-=======
->>>>>>> branch 'master' of https://github.com/AriGlockner/InfiniteRecharge2020-1.git
 	public void climbUp() {
 		leftClimbMotor.set(1.0);
 		rightClimbMotor.set(1.0);
@@ -54,15 +31,4 @@ public class ClimbRails extends SubsystemBase {
 		rightClimbMotor.set(0.0);
 		System.out.println("Stopped");
 	}
-<<<<<<< HEAD
-
-	/*@Override
-	protected void initDefaultCommand() {
-		// TODO Auto-generated method stub
-		m_leftClimbMotor.ClimbMotorSubsystem();
-		m_rightClimbMotor.ClimbMotorSubsystem();
-	}*/
-	
-=======
->>>>>>> branch 'master' of https://github.com/AriGlockner/InfiniteRecharge2020-1.git
 }
