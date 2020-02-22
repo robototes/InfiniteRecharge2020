29be142d76ca66cad25f0a10a5620623a5a61daa
s-panatulas <s-panatulas@52S500041285.it.bsd405.org>
2020-02-22 15:32:12 -0800

    Driver station oblog

--- src/main/java/frc/team2412/robot/Logging.java
+++ src/main/java/frc/team2412/robot/Logging.java
@@ -1,42 +1,35 @@
 package frc.team2412.robot;
 
 import io.github.oblarg.oblog.Loggable;
-import io.github.oblarg.oblog.annotations.Config;
 import io.github.oblarg.oblog.annotations.Log;
 
 public class Logging implements Loggable {
 
-	@Log
-	private String allianceName;
-	@Log
-	private int teamNumber = 2412;
-	@Log
-	private boolean testMatch;
-	@Log
-	private boolean officialMatch;
+	// 4 rows, 10 col
 
-	public Logging() {
+	private static final String green = "#7FFF00";
+	private static final String red = "#B22222";
+	private static final String white = "#FFFFFF";
+
+	// Goal Able
+	@Log.BooleanBox(colorWhenFalse = white, colorWhenTrue = green, columnIndex = 0, rowIndex = 0, height = 2)
+	@Log.BooleanBox(colorWhenFalse = white, colorWhenTrue = green, columnIndex = 2, rowIndex = 0, height = 2)
+	public static boolean outerGoalAble = false;
+	@Log.BooleanBox(colorWhenFalse = white, colorWhenTrue = green, columnIndex = 0, rowIndex = 0, height = 2)
+	public static boolean innerGoalAble = false;
+	// Goal Aimed
+	@Log.BooleanBox(colorWhenFalse = white, colorWhenTrue = green, columnIndex = 0, rowIndex = 0, height = 2)
+	@Log.BooleanBox(colorWhenFalse = white, colorWhenTrue = green, columnIndex = 2, rowIndex = 0, height = 2)
+	public static boolean outerGoalAimed= false;
+	@Log.BooleanBox(colorWhenFalse = white, colorWhenTrue = green, columnIndex = 0, rowIndex = 0, height = 2)
+	public static boolean innerGoalAimed = false;
+	
+
+	@Log
+	public static boolean hum = true;
+
+	public void periodic() {
 
 	}
 
-	@Config
-	public void setMatchType(boolean isTestMatch) {
-		if (isTestMatch = true) {
-			testMatch = true;
-			officialMatch = false;
-		} else {
-			officialMatch = true;
-			testMatch = false;
-		}
-	}
-
-	@Config
-	public void setAllianceName(String name) {
-		allianceName = name;
-	}
-
-	@Config
-	public void setTeanNumber(int teamNum) {
-		teamNumber = teamNum;
-	}
 }
