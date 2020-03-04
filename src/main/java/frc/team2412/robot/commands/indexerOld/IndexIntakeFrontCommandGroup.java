0b63cdcc97c99baa86d5af2b53a03550c64b2087
s-ivesp <s-ivesp@52S500041310.it.bsd405.org>
2020-03-03 17:41:16 -0800

    Moved M ore Folders

--- src/main/java/frc/team2412/robot/commands/indexerOld/IndexIntakeFrontCommandGroup.java
+++ src/main/java/frc/team2412/robot/commands/indexerOld/IndexIntakeFrontCommandGroup.java
@@ -9,7 +9,6 @@ import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SelectCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.team2412.robot.RobotState;
-import frc.team2412.robot.commands.indexer.IndexSwitchFourCommand;
 import frc.team2412.robot.subsystems.IndexerMotorSubsystem;
 import frc.team2412.robot.subsystems.IndexerSensorSubsystem;
 
