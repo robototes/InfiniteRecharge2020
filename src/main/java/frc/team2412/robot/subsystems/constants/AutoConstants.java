f183c4ffbf0658daf6efbe9e5dc3a900044878f0
Ion <Ion@Ion-PC.Home>
2021-02-25 17:16:19 -0800

    Fixed spacing

--- src/main/java/frc/team2412/robot/subsystems/constants/AutoConstants.java
+++ src/main/java/frc/team2412/robot/subsystems/constants/AutoConstants.java
@@ -16,24 +16,14 @@ import frc.team2412.robot.RobotMap;
 import frc.team2412.robot.subsystems.DriveBaseSubsystem;
 
 public class AutoConstants {
-
 	public static final double MAX_VOLTAGE = 12;
-
 	public static final boolean kGyroReversed = false;
-
 	public static final double ksVolts = 0.185;
-
 	public static final double kvVoltSecondsPerMeter = 0.0754;
-
 	public static final double kaVoltSecondsSquaredPerMeter = 0.0105;
-
 	public static final double kPDriveVel = 0.488;
-
 	public static final double kTrackwidthMeters = 0.5461; // Horizontal distance between wheels
-
-	public static final DifferentialDriveKinematics kDriveKinematics = new DifferentialDriveKinematics(
-
-			kTrackwidthMeters);
+	public static final DifferentialDriveKinematics kDriveKinematics = new DifferentialDriveKinematics(kTrackwidthMeters);
 
 	public static final double kMaxSpeedMetersPerSecond = 3; // Max speed we can drive
 
@@ -42,43 +32,25 @@ public class AutoConstants {
 	// Ramsete Controller Value
 
 	public static final double kRamseteB = 2; // makes a more straight curve
-
 	public static final double kRamseteZeta = 0.7; // limits the correction
-
 	public static final double controlPanelCutOff = 4.0;
-
 	public static final double inititationLineMeters = 3.048;
 
 	// Create a voltage constraint to ensure we don't accelerate too fast
 
-	public static DifferentialDriveVoltageConstraint autoVoltageConstraint = new DifferentialDriveVoltageConstraint(
-
-			new SimpleMotorFeedforward(ksVolts, kvVoltSecondsPerMeter, kaVoltSecondsSquaredPerMeter), kDriveKinematics,
-
-			10);
+	public static DifferentialDriveVoltageConstraint autoVoltageConstraint = new DifferentialDriveVoltageConstraint(new SimpleMotorFeedforward(ksVolts, kvVoltSecondsPerMeter, kaVoltSecondsSquaredPerMeter), kDriveKinematics,10);
 
 	// Create config for trajectory
-
-	public static TrajectoryConfig config = new TrajectoryConfig(kMaxSpeedMetersPerSecond,
-
-			kMaxAccelerationMetersPerSecondSquared)
-
+	public static TrajectoryConfig config = new TrajectoryConfig(kMaxSpeedMetersPerSecond,kMaxAccelerationMetersPerSecondSquared)
 					// Add kinematics to ensure max speed is actually obeyed
-
 					.setKinematics(kDriveKinematics)
-
 					// Apply the voltage constraint
-
 					.addConstraint(autoVoltageConstraint);
 
-	public static SimpleMotorFeedforward simpleMotorFeedforward = new SimpleMotorFeedforward(ksVolts,
-
-			kvVoltSecondsPerMeter, kaVoltSecondsSquaredPerMeter);
+	public static SimpleMotorFeedforward simpleMotorFeedforward = new SimpleMotorFeedforward(ksVolts,kvVoltSecondsPerMeter, kaVoltSecondsSquaredPerMeter);
 
 	public static RamseteController ramseteControlller = new RamseteController(kRamseteB, kRamseteZeta);
-
 	public static PIDController pidController = new PIDController(kPDriveVel, 0, 0);
-
 	public static DriveBaseSubsystem driveSub = RobotMap.m_robotContainer.m_driveBaseSubsystem;
 
 	public static Translation2d slalomPointOne = new Translation2d(0.868, -4.273);
