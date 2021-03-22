39544e440db9f1c3dd927cee9e934e0ad9014339
eddie <eddie@LAPTOP-O16FKU45>
2021-03-22 16:59:31 -0700

    Set the hood to a constant value for at home

--- src/main/java/frc/team2412/robot/subsystems/constants/HoodConstants.java
+++ src/main/java/frc/team2412/robot/subsystems/constants/HoodConstants.java
@@ -7,5 +7,8 @@ public class HoodConstants {
 
 	public static final double MAX_WITHDRAWL_ANGLE = 90;
 	public static final double MAX_EXTENSION_ANLGE = 0;
+	
+	// for infinite recharge at home
+	public static final double AT_HOME_ANGLE = 0.4;
 
 }
